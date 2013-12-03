86f8627226d6718c9cc1ab0ab4cada9ec95d1838
Andy Yao <yaogangqiang@admaster.com.cn>
2013-12-03 09:48:52 +0800

    add periodic_task

---
+++ periodic_task.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+import sched, time
+from functools import partial
+
+def periodic(delay_time, start_time):
+    def wrap(func):
+        def event(start_time, *args, **kwargs):
+            if time.time() > start_time:
+                func(*args, **kwargs)
+            params = tuple([start_time + delay_time] + list(args))
+            scheduler.enterabs(start_time + delay_time, 1, event, params, **kwargs)
+            scheduler.run()
+        scheduler = sched.scheduler(time.time, time.sleep)
+        return partial(event, start_time)
+    return wrap
+
+# params delay_time and start_time
+@periodic(2, time.time())
+def foo(num, a=1):
+    print num, a
+
+foo(9)
